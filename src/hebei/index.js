b4d7ab13c352e60b0771cbdd51299a54e263927b
GuoJikun <guojikun@y-open.com>
2021-12-08 17:00:49 +0800

    WIP:初始化

---
+++ src/hebei/index.js
@@ -0,0 +1,11 @@
+// 河北省
+import shijiazhuang from "./shijiazhuang";
+import tangshan from "./tangshan";
+import qinhuangdao from "./qinhuangdao";
+
+export default [
+  { code: "130000", name: "河北省" },
+  ...shijiazhuang,
+  ...tangshan,
+  ...qinhuangdao,
+];
---
+++ src/index.js
@@ -0,0 +1,5 @@
+export default [
+  { code: "710000", name: "台湾省" },
+  { code: "810000", name: "香港特别行政区" },
+  { code: "820000", name: "澳门特别行政区" },
+];
